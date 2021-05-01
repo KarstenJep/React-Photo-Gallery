30a34eecc572a5090b91983452301c0945f77de7
Karsten Jepsen <Kjepsen86@gmail.com>
2021-04-30 20:13:46 -0500

    Added GET, Header Component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,35 @@
+import Axios from 'axios';
 import React from 'react';
+import Header from '../Header/Header.jsx';
+
 import './App.css';
 
 function App() {
+  // Create storage for gallery list
+
+  // On page load, get gallery list
+
+  // Get gallery list via axios
+  const getGalleryList = () => {
+      axios.get('/gallery')
+        .then((response) => {
+          console.log(response.data);
+          //setGalleryList(response.data);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+  }
+
+
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Gallery of My Life</h1>
-        </header>
+        <Header />
+        
         <p>Gallery goes here</p>
         <img src="images/goat_small.jpg"/>
+        <img src="images/sam.jpeg" width="300"/>
+        <img src="images/Zion.jpg" width="300"/>
       </div>
     );
 }
